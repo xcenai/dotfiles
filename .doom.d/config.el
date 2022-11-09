f40dc907cac62018ffaefd9f5782e1baba02a26d
Aspectsides <64868985+Aspectsides@users.noreply.github.com>
2022-11-09 10:52:52 -0800

    removed some personal info keep it quiet guys

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,8 +6,6 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "Daniel Xu"
-      user-mail-address "danielthepig333@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
